674a0fe1dd1e0ead5e1aacfe9fd8e761035e775a
yhujeg <153836543+yhujeg@users.noreply.github.com>
2023-12-27 19:26:59 +0300

    Add files via upload

---
+++ hw-4.js
@@ -0,0 +1,52 @@
+// задание 1
+// let i = 0;
+
+// do {
+//     console.log("Привет");
+//     i++;
+// } while (i <= 1);
+
+// задание 2
+// let num = 1;
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 6);
+
+// задание 3
+// let number = 7;
+
+// do {
+//     console.log(number);
+//     number++;
+// } while (number <= 22);
+
+// задание 4
+// const obj = {
+//     'Коля': 200,
+//     'Вася': 300,
+//     'Петя': 400,
+// };
+// for (const key in obj) {
+// console.log(`${key} — зарплата ${obj[key]} долларов.`);
+// }
+
+// задание 5
+// let n = 1000;
+// let num = 0;
+
+// while(n >= 50) {
+//     n /= 2;
+//     num++
+// }
+// console.log(`Число ${n}`);
+// console.log(`Количество попыток ${num}`);
+
+// задание 6
+// const amountOfDays = 31;
+// for (let day = 1; day < amountOfDays; day+=7) {
+//     if ((day + 1) / 7) {
+//         console.log (`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+//     }
+// }
